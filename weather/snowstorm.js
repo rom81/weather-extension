51ef22c831f3e5392aa9d6546914f3302b7345ef
Sarah <shb66@pitt.edu>
2017-01-21 19:08:12 -0500

    new snowstorm

--- weather/snowstorm.js
+++ weather/snowstorm.js
@@ -15,7 +15,7 @@ var snowStorm = (function(window, document) {
 
   // --- common properties ---
 
-  this.autoStart = true;          // Whether the snow should start automatically or not.
+  this.autoStart = false;          // Whether the snow should start automatically or not.
   this.excludeMobile = true;      // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) Enable at your own risk.
   this.flakesMax = 128;           // Limit total amount of snow made (falling + sticking)
   this.flakesMaxActive = 64;      // Limit amount of snow falling at once (less = lower CPU use)
@@ -25,7 +25,7 @@ var snowStorm = (function(window, document) {
   this.excludeMobile = true;      // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) By default, be nice.
   this.flakeBottom = null;        // Integer for Y axis snow limit, 0 or null for "full-screen" snow effect
   this.followMouse = true;        // Snow movement can respond to the user's mouse
-  this.snowColor = '#99ccff';        // Don't eat (or use?) yellow snow.
+  this.snowColor = '#fff';        // Don't eat (or use?) yellow snow.
   this.snowCharacter = '&bull;';  // &bull; = bullet, &middot; is square on some systems etc.
   this.snowStick = true;          // Whether or not snow should "stick" at the bottom. When off, will never collect.
   this.targetElement = null;      // element which snow will be appended to (null = document.body) - can be an element ID eg. 'myDiv', or a DOM node reference
